f23b5d9ccd7d031b8e991eedc1beae799d709a3f
ansari-akram <ansariakramobaid@gmail.com>
2022-06-17 18:18:00 +0530

    compiled code

---
+++ compiled_code.py
@@ -0,0 +1,6 @@
+import py_compile
+
+py_compile.compile("multiple_currency_interval_v2.py")
+py_compile.compile("insert_table_prediction_v2.py")
+py_compile.compile("insert_table_prediction_buy_sell.py")
+py_compile.compile("buy_sell_v2.py")
